4c8f95434386ba310f439c75e3fdf7aeaa50569b
nicolas <ni.moragao@duocuc.cl>
2023-10-29 19:02:00 -0300

    creacion de calendario inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'perfil',
     loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
   },
+  {
+    path: 'tareas',
+    loadChildren: () => import('./tareas/tareas.module').then( m => m.TareasPageModule)
+  },
 
 ];
 
