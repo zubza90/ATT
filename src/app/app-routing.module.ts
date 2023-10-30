4853f589b21cb3d5bfc44998d8a7978a196bbc64
Jordan <jord.navarro@duocuc.cl>
2023-10-29 22:31:43 -0300

    Creacion de login,registro,resetpassword

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,26 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'landing',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },
+
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBQlAdEMg-MAwojiXAQVWwc0RbDdellmd0",
+    authDomain: "proyectoatt-c9de7.firebaseapp.com",
+    projectId: "proyectoatt-c9de7",
+    storageBucket: "proyectoatt-c9de7.appspot.com",
+    messagingSenderId: "381572704079",
+    appId: "1:381572704079:web:75bcf6380c0d9af791c73a",
+    measurementId: "G-2Y0NS7P4GH"
+  }
 };
 
 /*
